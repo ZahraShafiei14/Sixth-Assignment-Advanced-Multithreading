2222d4820925205518ffa1c7244b8af8e842f3bc
Zahra <zahrashafiei14@gmail.com>
2024-06-02 05:24:18 +0330

    pi calculator completed

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,27 +1,62 @@
 package sbu.cs.CalculatePi;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class PiCalculator {
 
-    /**
-     * Calculate pi and represent it as a BigDecimal object with the given floating point number (digits after . )
-     * There are several algorithms designed for calculating pi, it's up to you to decide which one to implement.
-     Experiment with different algorithms to find accurate results.
+    private static BigDecimal pi = new BigDecimal("0");
 
-     * You must design a multithreaded program to calculate pi. Creating a thread pool is recommended.
-     * Create as many classes and threads as you need.
-     * Your code must pass all of the test cases provided in the test folder.
+    private static class BBPAlgorithm implements Runnable {
 
-     * @param floatingPoint the exact number of digits after the floating point
-     * @return pi in string format (the string representation of the BigDecimal object)
-     */
+        private final int startPoint;
+        private final MathContext mc = new MathContext(1001);
+
+        public BBPAlgorithm(int startPoint) {
+            this.startPoint = startPoint;
+        }
+        @Override
+        public void run() {
+            BigDecimal sum = new BigDecimal("0");
+            for (int i = startPoint; i < startPoint + 50; i++) {
+                BigDecimal a = new BigDecimal("0.0625").pow(i);
+                BigDecimal b = new BigDecimal("4").divide(new BigDecimal((8*i+1)), mc);
+                BigDecimal c = new BigDecimal("-2").divide(new BigDecimal((8*i+4)), mc);
+                BigDecimal d = new BigDecimal("-1").divide(new BigDecimal((8*i+5)), mc);
+                BigDecimal e = new BigDecimal("-1").divide(new BigDecimal((8*i+6)), mc);
+                sum = sum.add(a.multiply(b.add(c, mc).add(d).add(e), mc));
+            }
+            Sum(sum);
+        }
+
+        private static synchronized void Sum(BigDecimal n) {
+            pi = pi.add(n);
+        }
+    }
 
     public String calculate(int floatingPoint)
     {
-        // TODO
-        return null;
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 20; i++) {
+            BBPAlgorithm num = new BBPAlgorithm(50 * i);
+            pool.execute(num);
+        }
+        pool.shutdown();
+        try {
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.getMessage();
+        }
+
+        return pi.toString().substring(0, floatingPoint + 2);
     }
 
     public static void main(String[] args) {
-        // Use the main function to test the code yourself
+        PiCalculator pi = new PiCalculator();
+        System.out.println(pi.calculate(2));
     }
 }
